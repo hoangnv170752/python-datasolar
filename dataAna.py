1d051d135b38d1a74a82bf1f3e37ee420821b108
Hoang Nguyen Van <hoang.nv.ral@gmail.com>
2023-08-12 21:03:31 +0700

    phan tich vi tri

--- dataAna.py
+++ dataAna.py
@@ -34,8 +34,42 @@ def phanTich():
             nhomtuoi["Duoi20"] += 1
         if 20 <= i <= 40:
             nhomtuoi["20-40"] += 1
+    plt.figure(figsize= (10,5))
     hang = list(nhomtuoi.keys()) 
     caccot = list(nhomtuoi.values())
+    ax = plt.gca()
+    label = caccot
+    bar = plt.bar(hang, caccot)
+    for idx,rect in enumerate(bar):
+        height = rect.get_height()
+        ax.text(rect.get_x() + rect.get_width()/2., 1.001*height,
+                list(label)[idx],
+                ha='center', va='bottom')
+    plt.title("Bieu do theo nhom tuoi nhan vien")
+    plt.ylabel("So luong")
+    plt.xlabel("Nhom tuoi")
+    plt.show()
+# phanTich()
+def vietTat(chuoi):
+    ketqua = chuoi[0]
+    while chuoi.find(" ") != -1:
+        vt = chuoi.find(" ")
+        ketqua = ketqua + chuoi[vt + 1].upper()
+        chuoi = chuoi[0:vt] + chuoi[vt + 1:]
+    return ketqua
+
+print(vietTat("Ki su phan mem"))
+
+def phanTichViTri():
+    nhomViTri = {}
+    for i in vitri:
+        i = vietTat(i)
+        if i not in nhomViTri: 
+            nhomViTri[i] = 0
+        nhomViTri[i] += 1
+    print(nhomViTri)
+    hang = list(nhomViTri.keys()) 
+    caccot = list(nhomViTri.values())
     ax = plt.gca()
     label = caccot
     bar = plt.bar(hang, caccot)
@@ -49,4 +83,4 @@ def phanTich():
     plt.ylabel("So luong")
     plt.xlabel("Nhom tuoi")
     plt.show()
-phanTich()
+phanTichViTri()
